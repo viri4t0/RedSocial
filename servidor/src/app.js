8eecfff5a5faa7a943113c6841812ba0d81802e7
lrr49 <luis_rr_@hotmail.com>
2020-03-05 22:17:16 +0100

    Login y registro con jwt
    Utiliza el modelo creado de user

---
+++ servidor/src/app.js
@@ -0,0 +1,57 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+
+mongoose.connect('mongodb://localhost/redsocial', {
+    //configuracion para evitar deprecates
+    //useMongoClient: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(db => console.log('DB CONNECTED'))
+    .catch(err => console.log(err));
+
+
+
+//esto pilla las peticiones antes de que lleguen a la ejecucion en el servidor
+//y te las muestra por consola
+app.use(morgan('dev'));
+//me permite usar los parametros de url y forms
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// ===========================
+// Puerto
+// ===========================
+
+process.env.PORT = process.env.PORT || 3000;
+
+// ===========================
+// Entorno
+// ===========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ===========================
+// Vencimiento de token
+// ===========================
+
+process.env.CADUCIDAD_TOKEN = '48h';
+
+// ===========================
+// SEED de autenticación
+// ===========================
+
+process.env.SEED_AUTENTICACION = process.env.SEED_AUTENTICACION || 'este-es-el-seed-desarrollo';
+
+//routes
+app.use(require('./routes/index.js'));
+
+
+app.listen(3000, () => {
+    console.log('Server on port 3000 running');
+});
--- servidor/src/routes/index.js
+++ servidor/src/routes/index.js
@@ -1,5 +1,8 @@
 const { Router } = require('express');
 const router = Router();
+const bcrypt = require('bcrypt');
+const Usuario = require('./../models/user');
+const jwt = require('jsonwebtoken'); 
 
 //estas son las rutas a las que puedes acceder
 router.get('/', (req, res) => {
@@ -8,12 +11,82 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/prueba', (req, res) => {
-    const data = {
-        "nombre" : "Luis",
-        "edad" : 12
-    }
-    res.json(data);
+router.post('/login', (req, res) => {
+    let body = req.body;
+
+    Usuario.findOne({ email: body.email }, (erro, usuarioDB)=>{
+        if (erro) {
+            return res.status(500).json({
+                ok: false,
+                err: erro
+            })
+        }
+
+        // Verifica que exista un usuario con el mail escrita por el usuario.
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: "Usuario o contraseña incorrectos"
+                }
+            })
+        } 
+
+        // Valida que la contraseña escrita por el usuario, sea la almacenada en la db
+        if (! bcrypt.compareSync(body.password, usuarioDB.password)){
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: "Usuario o contraseña incorrectos"
+                }
+            });
+        }
+
+        // Genera el token de autenticación
+        let token = jwt.sign({
+            usuario: usuarioDB,
+        }, process.env.SEED_AUTENTICACION, {
+            expiresIn: process.env.CADUCIDAD_TOKEN
+        })
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB,
+            token,
+        })
+
+    })
+    
 });
+
+router.post('/register', function (req, res) {
+    let body = req.body;
+
+    let { nombre, email, password, role } = body;
+    let usuario = new Usuario({
+      nombre,
+      email,
+      password: bcrypt.hashSync(password, 10),
+      role
+    });
+
+    usuario.save((err, usuarioDB) => {
+      if (err) {
+          return res.status(400).json({
+              ok: false,
+              err,
+          });
+      }
+
+      
+
+      res.json({
+          ok: true,
+          usuario: usuarioDB
+      });
+
+    })
+});
+
 
 module.exports = router;
